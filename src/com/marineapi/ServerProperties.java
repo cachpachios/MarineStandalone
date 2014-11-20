5a35bcee084faa5258c06407de6c4b93bf103aea
Fozie <foziegameing@gmail.com>
2014-11-20 22:16:48 +0100

    ServerStruc
    Changed a little bit

--- src/com/marineapi/ServerProperties.java
+++ src/com/marineapi/ServerProperties.java
@@ -7,6 +7,6 @@ public class ServerProperties {
 	
 	//FINAL VALUES
 	public static final int PROTOCOL_VERSION = 47;
-	public static final String Minecraft_Name = "1.8";
+	public static final String Minecraft_Name = "1.8.1";
 	public static final int MAX_Y = 256;
 }
--- src/com/marineapi/StandaloneServer.java
+++ src/com/marineapi/StandaloneServer.java
@@ -4,13 +4,10 @@ import com.marineapi.net.NetworkManager;
 
 public class StandaloneServer {
 	
-	public static final int PROTOCOL_VERSION = 47;
-	public static final String Minecraft_Name = "1.8";
-	
 	static NetworkManager network;
 	
 	public static void main(String[] args) {
-		Logging.getLogger().log("Marine Standalone Server Starting protocol version "+PROTOCOL_VERSION+" (Minecraft "+Minecraft_Name+")");
+		Logging.getLogger().log("Marine Standalone Server Starting protocol version "+ ServerProperties.PROTOCOL_VERSION +" (Minecraft "+ ServerProperties.Minecraft_Name +")");
 		network = new NetworkManager(25565); //TODO: Fix custom port!
 		network.openConnection();
 	}
