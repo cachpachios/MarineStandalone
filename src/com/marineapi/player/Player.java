cd7325e97ad7b2121e00ba1daba236e2cafd09e8
Fozie <foziegameing@gmail.com>
2014-11-20 22:14:42 +0100

    Players

--- src/com/marineapi/player/Player.java
+++ src/com/marineapi/player/Player.java
@@ -4,10 +4,8 @@ import java.util.UUID;
 
 import com.marineapi.net.Client;
 
-public class Player {
-	public Client connection;
+public class Player { //TODO: Extend LivingEntity
+	private Client connection;
+	private PlayerID id;
 	
-	public String displayName;
-	
-	public UUID uuid;
 }
