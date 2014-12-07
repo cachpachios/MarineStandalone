9240406c8557e64a64a29008909e1a225921e868
Sauilitired <citymonstret@hotmail.com>
2014-12-07 20:33:01 +0100

    JDoc update

--- src/com/marine/util/ListResponse.java
+++ src/com/marine/util/ListResponse.java
@@ -9,12 +9,39 @@ import org.json.simple.JSONArray;
  */
 public class ListResponse {
 
+    /**
+     * Message of the day
+     */
     public final String MOTD;
+
+    /**
+     * Current player count
+     */
     public final int CURRENT_PLAYERS;
+
+    /**
+     * Max player count
+     */
     public final int MAX_PLAYERS;
+
+    /**
+     * A sample list of players (can be used for messages)
+     */
     public JSONArray SAMPLE_PLAYERS;
+
+    /**
+     * Server favicon
+     */
     public String FAVICON;
 
+    /**
+     * Create a new list response
+     * @param motd Message of the day
+     * @param currentPlayers Current player count
+     * @param maxPlayers Max player count
+     * @param samplePlayers Sample player list
+     * @param favicon Server icon
+     */
     public ListResponse(String motd, int currentPlayers, int maxPlayers, JSONArray samplePlayers, String favicon) {
         this.MOTD = motd;
         this.CURRENT_PLAYERS = currentPlayers;
