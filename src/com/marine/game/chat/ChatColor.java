7eb5a3570c5caf9ae613d4bf08d3ecedcd83c7ca
Sauilitired <citymonstret@hotmail.com>
2014-12-07 12:12:30 +0100

    Improved ChatColor.java

--- src/com/marine/game/chat/ChatColor.java
+++ src/com/marine/game/chat/ChatColor.java
@@ -5,23 +5,23 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public enum ChatColor {
-	BLACK			("black",		 "0", true, "000000", Color.BLACK),
-    DARK_BLUE		("dark_blue",	 "1", true, "0000AA", Color.BLUE),
-    DARK_GREEN		("dark_green",	 "2", true, "00AA00", Color.GREEN),
-    DARK_AQUA		("dark_aqua",	 "3", true, "00AAAA", Color.CYAN),
-    DARK_RED		("dark_red",	 "4", true, "AA0000", Color.RED),
-    DARK_PURPLE		("dark_purple",	 "5", true, "AA0000", Color.MAGENTA),
-    GOLD			("gold",		 "6", true, "FFAA00", Color.ORANGE),
-    GRAY			("gray",		 "7", true, "AAAAAA", Color.GRAY),
-    DARK_GRAY		("dark_gray",	 "8", true, "555555", Color.DARK_GRAY),
-    BLUE			("blue",		 "9", true, "5555FF", Color.BLUE),
-    GREEN			("green",		 "a", true, "55FF55", Color.GREEN),
-    AQUA			("aqua",		 "b", true, "55FFFF", Color.CYAN),
-    LIGHT_BLUE		("aqua",		 "b", true, "55FFFF", Color.BLUE),
-    RED				("red",			 "c", true, "FF5555", Color.RED),
-    LIGHT_PURPLE	("light_purple", "d", true, "FF55FF", Color.MAGENTA),
-    YELLOW			("yellow",		 "e", true, "FFFF55", Color.YELLOW),
-    WHITE			("white",		 "f", true, "FFFFFF", Color.WHITE),
+	BLACK			("black",		 "0", true, "000000", Color.BLACK       ),
+    DARK_BLUE		("dark_blue",	 "1", true, "0000AA", Color.BLUE        ),
+    DARK_GREEN		("dark_green",	 "2", true, "00AA00", Color.GREEN       ),
+    DARK_AQUA		("dark_aqua",	 "3", true, "00AAAA", Color.CYAN        ),
+    DARK_RED		("dark_red",	 "4", true, "AA0000", Color.RED         ),
+    DARK_PURPLE		("dark_purple",	 "5", true, "AA0000", Color.MAGENTA     ),
+    GOLD			("gold",		 "6", true, "FFAA00", Color.ORANGE      ),
+    GRAY			("gray",		 "7", true, "AAAAAA", Color.GRAY        ),
+    DARK_GRAY		("dark_gray",	 "8", true, "555555", Color.DARK_GRAY   ),
+    BLUE			("blue",		 "9", true, "5555FF", Color.BLUE        ),
+    GREEN			("green",		 "a", true, "55FF55", Color.GREEN       ),
+    AQUA			("aqua",		 "b", true, "55FFFF", Color.CYAN        ),
+    LIGHT_BLUE		("aqua",		 "b", true, "55FFFF", Color.BLUE        ),
+    RED				("red",			 "c", true, "FF5555", Color.RED         ),
+    LIGHT_PURPLE	("light_purple", "d", true, "FF55FF", Color.MAGENTA     ),
+    YELLOW			("yellow",		 "e", true, "FFFF55", Color.YELLOW      ),
+    WHITE			("white",		 "f", true, "FFFFFF", Color.WHITE       ),
     OBFUSCATED		("obfuscated",	 "k", false),
     BOLD			("bold",		 "l", false),
     STRIKETHROUGH	("strikethrough","m", false),
@@ -37,6 +37,7 @@ public enum ChatColor {
     private final boolean isColor;
     private final boolean isFormat;
     private final String hexa;
+    private final String rawID;
 	
 	private ChatColor(String rawData, String id, boolean isColor, String hexa, Color color) {
 		this.packetData = rawData;
@@ -45,6 +46,7 @@ public enum ChatColor {
         this.isColor = isColor;
         this.isFormat = !isColor && !id.equals("r");
         this.hexa = hexa;
+        this.rawID = id;
 	}
 
     private ChatColor(String rawData, String id, boolean isColor) {
@@ -54,6 +56,7 @@ public enum ChatColor {
         this.isColor = isColor;
         this.isFormat = !isColor && !id.equals("r");
         this.hexa = "";
+        this.rawID = id;
     }
 
     public static ChatColor getColor(char c) {
@@ -69,7 +72,7 @@ public enum ChatColor {
 	}
 	
 	public String getOldSystemID() {
-		return new String(new char[] {id});
+		return rawID;
 	}
 	
 	public char getOldSystemIDChar() {
@@ -132,6 +135,6 @@ public enum ChatColor {
 
     @Override
 	public String toString() {
-		return "" + COLOR_CHARACTER + id;
+		return COLOR_CHARACTER + rawID;
 	}
 }
