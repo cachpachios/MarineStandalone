38f2c1077ce3552e935d86b5a5387377942c627e
Sauilitired <citymonstret@hotmail.com>
2014-12-11 13:55:57 +0100

    I am even stupider than I thought

--- src/main/java/com/marine/game/chat/ChatColor.java
+++ src/main/java/com/marine/game/chat/ChatColor.java
@@ -65,7 +65,7 @@ public enum ChatColor {
      */
     public static String[] constants() {
         return new String[] {
-                black, dark_purple, dark_green, dark_aqua,
+                black, dark_blue, dark_green, dark_aqua,
                 dark_red, dark_purple, gold, gray, dark_gray,
                 blue, green, aqua, red, light_purple, yellow,
                 white, obfuscated, bold, strikethrough, underline,
