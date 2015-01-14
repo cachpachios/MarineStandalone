afc0660ab9f97cb518a7ef5a621aed5336214ad5
Fozie <foziegameing@gmail.com>
2015-01-14 16:59:03 +0100

    MathOperations

--- src/main/java/org/marinemc/util/MathUtils.java
+++ src/main/java/org/marinemc/util/MathUtils.java
@@ -1,5 +1,9 @@
 package org.marinemc.util;
 
+import org.marinemc.util.operations.ArgumentedOperation;
+import org.marinemc.util.vectors.Vector2d;
+import org.marinemc.util.vectors.Vector2i;
+
 public class MathUtils {
 	@SuppressWarnings("hiding")
 	public static <T extends Comparable<T>, Number> T trim(final T v,
@@ -24,5 +28,29 @@ public class MathUtils {
 
 		return (w < x || w > X) && (h < y || h > Y);
 	}
-
+	
+	public static void circleOperation(final ArgumentedOperation<Vector2d> op, int x, int y, int radius, int step) {
+		int theta = 0;
+		
+		while(theta <= 360) {
+			op.action(new Vector2d(x + radius*Math.cos(theta), y + radius*Math.sin(theta)));
+			theta += step;
+		}
+	}
+	
+	public static void circleOperation(final ArgumentedOperation<Vector2d> op, int x, int y, int radius) {
+		int theta = 0;
+		
+		while(theta++ <= 360)
+			op.action(new Vector2d(x + radius*Math.cos(theta), y + radius*Math.sin(theta)));
+	}
+	
+	public static void rectOperation(final ArgumentedOperation<Vector2i> op, final int X, final int Y, final int width, final int height) {
+		for(int x = width * -1; x < width; x++)
+			for(int y = height * -1; y < height; y++) {
+					op.action(new Vector2i(X+x, Y+y));
+			}
+	}
+	
+ 
 }
